aa8b9dd012f93668f132c1dc74e22aa64efad2f1
Jacob Bryant <jacob.bryant1@maine.edu>
2024-09-16 09:29:10 -0400

    Pass all the tests

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,32 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jacob Bryant. Hello World.
             </p>
+
+            <h1>Cat</h1>
+
+            <img
+                width="500"
+                height="500"
+                src="https://cataas.com/cat"
+                alt="cat"
+            ></img>
+
+            <div className="App-list" style={{ backgroundColor: "red" }}>
+                <ul>
+                    <li>Cake</li>
+                    <li>Lobster</li>
+                    <li>Pie</li>
+                </ul>
+            </div>
+
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </button>
         </div>
     );
 }
