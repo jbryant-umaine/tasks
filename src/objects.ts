846254ea6f2a4e9b5e3c50a9be00f22bd9867c8e
Jacob Bryant <jacob.bryant1@maine.edu>
2024-09-27 21:20:10 -0400

    Solve publishQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -102,7 +102,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    let newQuestion = { ...question, published: !question.published };
+
+    return newQuestion;
 }
 
 /**
