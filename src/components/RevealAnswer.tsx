c3dfcfa0044b8ef992a2a4879af9a57fd269022b
Jacob Bryant <jacob.bryant1@maine.edu>
2024-10-15 19:48:14 -0400

    Solve RevealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,23 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [answerShown, showAnswer] = useState<boolean>(false);
+
+    return (
+        <div>
+            {answerShown && (
+                <div>
+                    The answer is <span data-testid="question-answer">42</span>.
+                </div>
+            )}
+            <Button
+                onClick={() => {
+                    showAnswer(!answerShown);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            ;
+        </div>
+    );
 }
