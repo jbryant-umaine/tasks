a1a6562aec10998b78358dd1ad8a8faef69c954e
Jacob Bryant <jacob.bryant1@maine.edu>
2024-10-15 20:05:36 -0400

    Solve ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -2,6 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
+const QUESTION_TRANSITIONS: Record<QuestionType, QuestionType> = {
+    ["short_answer_question"]: "multiple_choice_question",
+    ["multiple_choice_question"]: "short_answer_question",
+};
+
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question",
+    );
+
+    const buttonText =
+        questionType === "short_answer_question" ? "Short Answer" : (
+            "Multiple Choice"
+        );
+
+    return (
+        <div>
+            <div>{buttonText}</div>
+            <Button
+                onClick={() => {
+                    setQuestionType(QUESTION_TRANSITIONS[questionType]);
+                }}
+            >
+                Change Type
+            </Button>
+        </div>
+    );
 }
