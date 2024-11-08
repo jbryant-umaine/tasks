7a510465cdc52c27e7b2760fa195afc95c8143a8
Jacob Bryant <jacob.bryant1@maine.edu>
2024-11-08 06:58:39 -0500

    Solve DoubleHalf

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
+interface dhProps {
+    // The type is "a function that consumes a boolean and returns nothing"
+    setDhValue: (newDhvalue: number) => void;
+    dhValue: number;
+}
+
+function Doubler({ setDhValue, dhValue }: dhProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -14,7 +19,7 @@ function Doubler(): React.JSX.Element {
     );
 }
 
-function Halver(): React.JSX.Element {
+function Halver({ setDhValue, dhValue }: dhProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -27,14 +32,16 @@ function Halver(): React.JSX.Element {
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setDhValue={setDhValue} dhValue={dhValue}></Doubler>
+            <Halver setDhValue={setDhValue} dhValue={dhValue}></Halver>
         </div>
     );
 }
