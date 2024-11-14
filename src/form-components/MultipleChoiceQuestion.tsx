77d2e762515732d2f76a6fefb9e5baed8c9e1be7
Jacob Bryant <jacob.bryant1@maine.edu>
2024-11-14 08:35:34 -0500

    Solve MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [option, setOption] = useState<string>(options[0]);
+
+    function updateOption(event: React.ChangeEvent<HTMLSelectElement>) {
+        setOption(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="questions">
+                <Form.Label>Select Answer</Form.Label>
+                <Form.Select value={option} onChange={updateOption}>
+                    {options.map((color: string) => (
+                        <option key={color} value={color}>
+                            {color}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <h3>Answer {expectedAnswer === option ? "✔️" : "❌"}</h3>
         </div>
     );
 }
